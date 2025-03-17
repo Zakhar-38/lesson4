5731a2af281c495c7f4f92ec45badd262f285f2a
Zakhar-38 <28zaxarka@mail.ru>
2025-03-17 15:37:55 +0300

    Add Post Endpoint

--- main.js
+++ main.js
@@ -7,34 +7,7 @@ const cors = require('cors')
 const PORT = process.env.PORT || 8080
 const app = express()
 const router = require('./routes/index')
-const {DataTypes} = require ('sequelize')
 
-const {Sequelize} = require ('sequelize')
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-      dialect:'postgres',
-      host: process.env.DB_host,
-      port: process.env.DB_port,
-  }
-)
-
-const ToDo = sequelize.define('todo',{
-  id:{type: DataTypes.INTEGER, primaryKey: true},
-  title:{type: DataTypes.STRING},
-  description: {type: DataTypes.STRING},
-  isDone:{type: DataTypes.BOOLEAN},
-})
-
-class DataController{
-  async create(req,res){
-      const{id,title,description,isDone}=req.body
-      const type = await ToDo.create({id,title,description,isDone})
-      return res.json(type)
-  }}
-  module.exports = new DataController()
 
 app.use(express.json())
 app.use('/api', router)
