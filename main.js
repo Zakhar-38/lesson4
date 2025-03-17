14bb0478f742b80b35df0cfdd3a343c3dd1ac274
Zakhar-38 <28zaxarka@mail.ru>
2025-03-17 14:04:09 +0300

    add web server

---
+++ main.js
@@ -0,0 +1,31 @@
+const express = require("express")
+require('dotenv').config()
+const sequelize = require('./db')
+const models = require('./models/models')
+const CryptoJS = require('crypto-js')
+const cors = require('cors')
+const PORT = process.env.PORT || 3100
+const app = express()
+const router = require('./routes/index')
+
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
+
+app.get('/', (req, res)=>{
+  res.status(200).json({message:'Hello world'})
+})
+
+const start = async () => {
+  try{   
+    let passwd = CryptoJS.SHA256("test")
+    console.log(passwd.toString(CryptoJS.enc.Hex));
+    await sequelize.authenticate()
+    await sequelize.sync()
+    app.listen(PORT, () => console.log(`Server start on ${PORT}`))
+  }  
+  catch(e){
+    console.log(e)
+  }
+}
+start()
