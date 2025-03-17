abf3c008b7b04b60ec44ca17b05d0696368ec111
Zakhar-38 <28zaxarka@mail.ru>
2025-03-17 14:13:05 +0300

    Add get Endpoint

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use('/api', router)
 
 app.get('/', (req, res)=>{
-  res.status(200).json({message:'hello world'})
+  res.status(200).json({message:'juriev 11-ES322'})
 })
 
 const start = async () => {
