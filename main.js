af1fb1d72d499a6f83662b1e4d6a0db0fd36c4f4
Zakhar-38 <28zaxarka@mail.ru>
2025-03-17 14:49:20 +0300

    Add delete Endpoint

--- main.js
+++ main.js
@@ -7,14 +7,50 @@ const cors = require('cors')
 const PORT = process.env.PORT || 8080
 const app = express()
 const router = require('./routes/index')
+const {DataTypes} = require ('sequelize')
 
-app.use(cors())
+const {Sequelize} = require ('sequelize')
+module.exports = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+      dialect:'postgres',
+      host: process.env.DB_host,
+      port: process.env.DB_port,
+  }
+)
+
+const ToDo = sequelize.define('todo',{
+  id:{type: DataTypes.INTEGER, primaryKey: true},
+  title:{type: DataTypes.STRING},
+  description: {type: DataTypes.STRING},
+  isDone:{type: DataTypes.BOOLEAN},
+})
+
+class DataController{
+  async create(req,res){
+      const{id,title,description,isDone}=req.body
+      const type = await ToDo.create({id,title,description,isDone})
+      return res.json(type)
+  }}
+  module.exports = new DataController()
+
 app.use(express.json())
 app.use('/api', router)
 
-app.get('/', (req, res)=>{
-  res.status(200).json({message:'juriev 11-ES322'})
+
+
+
+
+  
+app.delete('/deleteToDoWhere', (req, res) => {
+  const { id, title, description, isDone } = req.body
+  let Delete_ID = Number(req.params.Delete_ID)
+  const type = User.destroy({ where: { id: Delete_ID } });
+  res.send("!")
 })
+
 
 const start = async () => {
   try{   
